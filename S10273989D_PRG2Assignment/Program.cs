5200dd536dcb5a13d25c5da0c7f2d3a13b269515
YizehnC <yizehn.choo@gmail.com>
2026-01-29 14:54:33 +0800

    Customer and Order Initialization

--- S10273989D_PRG2Assignment/Program.cs
+++ S10273989D_PRG2Assignment/Program.cs
@@ -5,11 +5,12 @@ using S10273989D_PRG2Assignment;
 
 List<Restaurant> restaurantsObj = new List<Restaurant>();
 List <Customer> customerObj = new List<Customer>();
+List<Order> orderObj = new List<Order>();
 
 
 void RestaurantInit()
 {
-    using (StreamReader sr = new StreamReader("resraurant.csv"))
+    using (StreamReader sr = new StreamReader("restaurants.csv"))
     {
 
         string title = sr.ReadLine();
@@ -32,125 +33,6 @@ void RestaurantInit()
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 int CustomerInit()
 {
     int customerCount = 0;
@@ -193,10 +75,143 @@ int OrderInit()
             {
                 orderCount++;
                 string[] orderInfo = line.Split(',');
-
+                Order ord = new Order(orderInfo[0], orderInfo[1], orderInfo[2], orderInfo[3], orderInfo[4], orderInfo[5], orderInfo[6], orderInfo[7], orderInfo[8], orderInfo[9]);
+                orderObj.Add(ord);
+                foreach (Order in orderObj)
+                {
+                    foreach (Customer cust in customerObj)
+                    {
+                        if (ord.CustomerEmail == cust.EmailAddress)
+                        {
+                            cust.AddOrder(ord);
+                        }
+                    }
+                    foreach (Restaurant res in restaurantsObj)
+                    {
+                        if (ord.RestaurantID == res.RestaurantId)
+                        {
+                            res.Order.Add(ord);
+                        }
+                    }
+                }
 
             }
         }
         return orderCount;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
